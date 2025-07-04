3added45f25bcb9d5c39af95d00243ac97b09cc9
takshabansal <takshabansal@gmail.com>
2025-07-04 18:03:08 +0530

    This is the After class project for session 2

---
+++ After class project.py
@@ -0,0 +1,6 @@
+Bday1="15th June"
+Bday2="9th February"
+Bday3="8th January"
+Bday4="25th November"
+Bday5="25th April"
+print("Hello ,my birthday falls on",Bday1,".My mother's birthday falls on",Bday2,".My father's birthday falls on",Bday3,".My friend's birthdays fall on",Bday4,"and",Bday5)
