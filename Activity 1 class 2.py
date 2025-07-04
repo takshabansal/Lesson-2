affc70572fc99bc1143dc0e1a0b6eb65fd3f6cd6
takshabansal <takshabansal@gmail.com>
2025-07-04 17:40:22 +0530

    Lesson 2 activities

---
+++ Activity 1 class 2.py
@@ -0,0 +1,11 @@
+#print a sentence
+print("Welcome to the world of programing.")
+
+#printing a number
+print(15)
+#next line after print
+print("Hello python\n")
+#pass multiple arguements
+print("Hello",6)
+#end arguement of print statement
+print("Welcome to",end=" @")
---
+++ Activity 2.py
@@ -0,0 +1,11 @@
+#creating different variables
+num1=15
+word="Taksha"
+print(num1)
+print(word)
+print(num1,word)
+codingal="codingal"
+print(codingal)
+#user input
+name=input("Enter your name beside this message")
+print("\nHello",name,"Welcome to codingal")
